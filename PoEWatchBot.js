b746704a18020e96b9d2cb349ec022edcf08fba8
Vertex101 <vertex101@gmx.com>
2020-01-20 05:34:29 -0500

    added in chaos command plus other odds and ends

--- PoEWatchBot.js
+++ PoEWatchBot.js
@@ -2,7 +2,8 @@ const config = require('./config/cfg.json');
 const tmi = require('tmi.js');
 const request = require('request');
 const fs = require('fs');
-var gem2120 = [], gem2123 = []
+var gem2120 = [], gem2123 = [], gemAwake = []
+var prefix = "!";
 let options = {
     options: {
         debug: true
@@ -21,7 +22,7 @@ let client = new tmi.client(options);
 // Connect the client to the server..
 client.connect();
 //join the channels from the json file
-client.on("connected", (address, port) => {
+client.on("connected", async (address, port) => {
     //TODO: loop through the json file to connect to the channels
     fs.readFile('config/channel.json', (err, data) => {  
         if (err) throw err;
@@ -33,19 +34,19 @@ client.on("connected", (address, port) => {
         }
     });
 });
-client.on("chat", (channel, user, message, self) => {
-    // Don't listen to my own messages..
+client.on("chat", async (channel, user, message, self) => {
     if (self) return;
-    // Do your stuff.
-    msg = message.split(" ");
-    //PoE command things
+
+    if(message.indexOf(prefix) !== 0) return;
+    const args = message.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
     if(user.username == channel.replace("#", "") ||  user.mod || user.username == "vertex101"){
-        if(msg[0] == "!cmds") {
+        if(command == "cmds") {
             setTimeout(function () {
-                client.say(channel, "Current Commands: !ex, !23, !20, !hunter, !doc, !mirror, !round")
+                client.say(channel, "Current Commands: !ex, !23, !20, !hunter, !doc, !mirror, !round, !chaos, !starter")
             }, 3000); 
         }
-        if(msg[0] == "!ex") {
+        if(command == "ex") {
             request('https://api.poe.watch/item?id=142', function (error, response, body) {
                 pullData = JSON.parse(body);
                 setTimeout(function () {
@@ -53,7 +54,7 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000); 
             });
         }
-        if(msg[0] == "!23") {
+        if(command == "23") {
             request("https://api.poe.watch/get?league=Metamorph&category=gem", function (error, responce, body) {
                 top523 = JSON.parse(body);
                 top523.forEach(function (fruit) {
@@ -76,7 +77,30 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000)
             });
         }
-        if(msg[0] == "!20") {
+        if(command == "awakened") {
+            request("https://api.poe.watch/get?league=Metamorph&category=gem", function (error, responce, body) {
+                topAwak = JSON.parse(body);
+                topAwak.forEach(function (fruit) {
+                    if(fruit.name.includes("Awakened") && fruit.gemQuality == "20") { // && fruit.change != "0"
+                    gemAwake.push(fruit.name+":"+fruit.exalted.toFixed(2))
+                    }
+                });
+                setTimeout(function() {
+                    var gem1 = gemAwake[0].split(":")
+                    var gem2 = gemAwake[1].split(":")
+                    var gem3 = gemAwake[2].split(":")
+                    var gem4 = gemAwake[3].split(":")
+                    var gem5 = gemAwake[4].split(":")
+                    client.say(channel, "Top 5 Awakened GEMS 1) "
+                        + gem1[0] + " - " + gem1[1]
+                        + "ex 2) " + gem2[0] + " - " + gem2[1]
+                        + "ex 3) " + gem3[0] + " - " + gem3[1]
+                        + "ex 4) " + gem4[0] + " - " + gem4[1]
+                        + "ex 5) " + gem5[0] + " - " + gem5[1] + "ex")
+                }, 3000)
+            });
+        }
+        if(command == "20") {
             request("https://api.poe.watch/get?league=Metamorph&category=gem", function (error, responce, body) {
                 top520 = JSON.parse(body);
                 top520.forEach(function (fruit) {
@@ -99,7 +123,7 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000)
             });
         }
-        if(msg[0] == "!hunter") {
+        if(command == "hunter") {
             request('https://api.poe.watch/item?id=3891', function (error, response, body) {
                 pullData = JSON.parse(body);
                 setTimeout(function () {
@@ -107,7 +131,7 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000); 
             });
         }
-        if(msg[0] == "!doc") {
+        if(command == "doc") {
             request('https://api.poe.watch/item?id=3509', function (error, response, body) {
                 pullData = JSON.parse(body);
                 setTimeout(function () {
@@ -115,7 +139,7 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000); 
             });
         }
-        if(msg[0] == "!mirror") {
+        if(command == "mirror") {
             request('https://api.poe.watch/item?id=3283', function (error, response, body) {
                 pullData = JSON.parse(body);
                 setTimeout(function () {
@@ -123,13 +147,13 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000); 
             });
         }
-        if(msg[0] == "!round") {
-            if(msg[1]) {
+        if(command == "round") {
+            if(args[0]) {
                 request('https://api.poe.watch/item?id=142', function (error, response, body) {
                     pullData = JSON.parse(body);
-                    var cTotal = (pullData.leagues[0].mode * Number("0." + msg[1] + "0"))
+                    var cTotal = (pullData.leagues[0].mode * Number("0." + args[0] + "0"))
                     setTimeout(function () {
-                        client.say(channel, "0." + msg[1] + "ex is " + Math.round(cTotal) + "c")
+                        client.say(channel, "0." + args[0] + "ex is " + Math.round(cTotal) + "c")
                     }, 3000); 
                 });
             } else {
@@ -138,28 +162,83 @@ client.on("chat", (channel, user, message, self) => {
                 }, 3000);
             }
         }
-        if(msg[0] == "!coc") { //https://www.youtube.com/watch?v=UqC4WM7am20
+        if(command == "chaos") {
+            if(args[0]) {
+                request('https://api.poe.watch/item?id=142', function (error, response, body) {
+                    pullData = JSON.parse(body);
+                    var cTotal = (Number(args[0]) / pullData.leagues[0].mode)
+                    var getOdds = cTotal.toFixed(2).split('.')
+                    console.log(getOdds)
+                    request('https://api.poe.watch/item?id=142', function (error, response, body) {
+                        pullData = JSON.parse(body);
+                        var cChaos = (pullData.leagues[0].mode * Number("0." + getOdds[0]))
+                        setTimeout(function () {
+                            client.say(channel, args[0]+"c = " + getOdds[0] + "ex " + cChaos + "c")
+                        }, 3000); 
+                    });
+                });
+            } else {
+                setTimeout(function () {
+                    client.say(channel, "Usage: !chaos [amount]")
+                }, 3000);
+            }
+        }
+        if(command == "starter") { //https://www.youtube.com/watch?v=2JPVJIn98B4
             setTimeout(function () {
-                client.say(channel, "Cast on Critical Strike Assassin [Cyclone] by Esoro https://www.youtube.com/watch?v=UqC4WM7am20 the PoB is in the video description")
+                client.say(channel, "New to PoE (Path of Exile) go here https://www.youtube.com/watch?v=2JPVJIn98B4 and watch Beginner Guide + Zizaran's Tips and Tricks")
             }, 3000);
         }
     }
     if(user.username == "vertex101") {
-        if(msg[0] == "!vso") {
-            request({ url: 'https://api.twitch.tv/kraken/users?login=' + msg[1].toLowerCase(), headers: { 'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': config.twitch.client}}, function (error, response, body) {
-                pullData = JSON.parse(body);
-                request({ url: 'https://api.twitch.tv/kraken/channels/' + pullData.users[0]._id, headers: { 'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': config.twitch.client}}, function (error, response, body) {
+        if(command == "vso") {
+            if(args[0]) {
+                request({ url: 'https://api.twitch.tv/kraken/users?login=' + args[0].toLowerCase(), headers: { 'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': config.twitch.client}}, function (error, response, body) {
                     pullData = JSON.parse(body);
-                    setTimeout(function () {
-                        client.say(channel, "You should 100% check out " + msg[1] + "! You can find them," +
-                        " here >> https://twitch.tv/" + msg[1].toLowerCase() + " <3 They were last playing >> " + pullData.game + "!")
-                    }, 3000);
+                    request({ url: 'https://api.twitch.tv/kraken/channels/' + pullData.users[0]._id, headers: { 'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': config.twitch.client}}, function (error, response, body) {
+                        pullData = JSON.parse(body);
+                        setTimeout(function () {
+                            client.say(channel, "You should 100% check out " + pullData.display_name + "! You can find them," +
+                            " here >> https://twitch.tv/" + pullData.name + " <3 They were last playing >> " + pullData.game + "!")
+                        }, 3000);
+                    });
                 });
-            });
+            }
         }
-        if(msg[0] == "!bpop") { //
+        if(command == "bpop") { //
             setTimeout(function () {
                 client.say(channel, "Bane POP build by mbXtreme https://www.youtube.com/watch?v=RDJqEdWqdAE the PoB is in the video description")
+            }, 3000);
+        }
+    }
+    if(channel == "#sketch") {
+        if(command == "gfg") { 
+            setTimeout(function () {
+                client.say(channel, 
+                    "Gamers for Giving is a weekend-long competitive gaming tournament,"+
+                    "LAN party, & streamathon that helps raise money in support of Gamers Outreach programs."+
+                    "Ticket sales and donations help provide entertainment devices to hospitalized children."+
+                    " to learn more go to https://gamersforgiving.org/")
+            }, 3000);
+        }
+        if(command == "go") { 
+            setTimeout(function () {
+                client.say(channel, 
+                    "What is Gamers Outreach?"+
+                    "Founded in 2007, we're a 501(c)(3) charity organization that provides equipment,"+
+                    "technology, and software to help kids cope with treatment inside hospitals."+
+                    " to learn more go to https://gamersoutreach.org/")
+            }, 3000);
+        }
+    }
+    if(channel == "#finncapp") {
+        if(command == "coc") { 
+            setTimeout(function () {
+                client.say(channel, 
+                    "CoC Ice Nova Assassin" +
+                    " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" +
+                    " currently running https://www.youtube.com/watch?v=UqC4WM7am20 <-- by Esoro" +
+                    " and working torwards https://www.youtube.com/watch?v=lgBaGtv2w6s <-- by Zenn" +
+                    " the PoB is in the video descriptions")
             }, 3000);
         }
     }
