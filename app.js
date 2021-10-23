9236bfeec3fdd7f24e1ba04a17469ae69c9f48ab
vertex101 <vertex101@gmx.com>
2021-10-23 00:52:48 -0400

    Update app.js
    updated for new league

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 var prefix = "?";
-var newLeague = "Expedition";
+var newLeague = "Scourge";
 let chaosPrice = "";
 
 const modList = ["finncapp", "vertex101"];
